02d93722d9da64667578f569d5f684949c362a7f
colorhook <colorhook@gmail.com>
2025-04-08 19:08:10 +0800

    fix: compile error in layout

--- src/app/[variants]/(main)/search/(content)/_layout/Desktop/index.tsx
+++ src/app/[variants]/(main)/search/(content)/_layout/Desktop/index.tsx
@@ -2,14 +2,11 @@ import { Flexbox } from 'react-layout-kit';
 
 import { LayoutProps } from '../type';
 
-const Layout = ({ children, modal }: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <Flexbox flex={1} style={{ overflow: 'hidden', position: 'relative' }}>
-        {children}
-      </Flexbox>
-      {modal}
-    </>
+    <Flexbox flex={1} style={{ overflow: 'hidden', position: 'relative' }}>
+      {children}
+    </Flexbox>
   );
 };
 
--- src/app/[variants]/(main)/search/(content)/_layout/type.ts
+++ src/app/[variants]/(main)/search/(content)/_layout/type.ts
@@ -3,5 +3,4 @@ import { ReactNode } from 'react';
 export interface LayoutProps {
   children: ReactNode;
   menu: ReactNode;
-  modal: ReactNode;
 }
